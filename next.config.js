0b77d118fc67b40a211dd8a274d2852863c48467
Hathim0001 <mohammedhathim0001@gmail.com>
2025-03-31 22:48:25 +0530

    adding supabase url and key

---
+++ next.config.js
@@ -0,0 +1,6 @@
+module.exports={
+    env:{
+        SUPABASE_URL:"https://mcjottryjrfflunqbmhi.supabase.co",
+        SUPABASE_ANON_KEY:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jam90dHJ5anJmZmx1bnFibWhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0Mzk4NzUsImV4cCI6MjA1OTAxNTg3NX0.PTcLJrARyNtniNPTl0JeKCeRR9rxEaLq6Ul6lHdWEr8"
+    }
+}

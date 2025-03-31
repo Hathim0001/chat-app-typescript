015a2391705fb21aa28ff6e99839a1d511567519
Hathim0001 <mohammedhathim0001@gmail.com>
2025-04-01 00:52:36 +0530

    final

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-module.exports={
-    env:{
-        SUPABASE_URL:"https://mcjottryjrfflunqbmhi.supabase.co",
-        SUPABASE_ANON_KEY:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jam90dHJ5anJmZmx1bnFibWhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0Mzk4NzUsImV4cCI6MjA1OTAxNTg3NX0.PTcLJrARyNtniNPTl0JeKCeRR9rxEaLq6Ul6lHdWEr8"
-    }
-}
+module.exports = {
+    reactStrictMode: true,
+    env: {
+      NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    },
+  };
+  
